9589307860db3476af1b9ee1f617cd9c5887a70b
mqamarmunir <qamar.nust@gmail.com>
2016-08-18 08:50:07 +0500

    Initial Commit Added Projects

---
+++ AttendenceSystem/AttendenceWS/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AttendenceWS.Models
+{
+    public class Person
+    {
+        public int UserId { get; set; }
+        public string Login { get; set; }
+        public string Org_BranchId { get; set;}
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+        public string Designation { get; set; }
+        public byte[] FingerPrint { get; set;}
+
+        public byte[] Picture { get; set; }
+
+    }
+}
